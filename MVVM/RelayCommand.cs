825c2ec2c419acfa23ff87852ecc7f54f26e4e2e
Mark Bell <skrambelled@gmail.com>
2024-10-03 11:05:55 -0700

    RelayCommand implementation

---
+++ MVVM/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Input;
+
+namespace DemoProject.MVVM
+{
+    class RelayCommand : ICommand
+    {
+        private Action<object> _execute;
+        private Func<object, bool>? _canExecute;
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove {  CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute,
+                            Func<object, bool>? canExecute = null)
+        {
+            this._execute = execute;
+            this._canExecute = canExecute;
+        }
+
+        // Assume we can execute if no function for _canExecute is provided,
+        // otherwise we can ask the provided Func with the supplied parameter
+        // if we can execute.
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute == null || CanExecute(parameter);
+        }
+
+        // Invoke the supplied Action
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
