797fe38b15ed29022452541980eb196d884007a2
Mark Bell <skrambelled@gmail.com>
2024-10-03 16:19:12 -0700

    MVVM for AddDataWindowModel

--- View/AddDataWindow.xaml.cs
+++ View/AddDataWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DemoProject.ViewModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace DemoProject.View
 {
@@ -22,6 +11,8 @@ namespace DemoProject.View
         public AddDataWindow()
         {
             InitializeComponent();
+            AddDataWindowViewModel vm = new AddDataWindowViewModel();
+            DataContext = vm;
         }
     }
 }
---
+++ ViewModel/AddDataWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using DemoProject.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject.ViewModel
+{
+    class AddDataWindowViewModel : ViewModelBase
+    {
+    }
+}
