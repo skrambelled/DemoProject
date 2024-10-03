04b040092962e80b2237b770ecae22b9976bb9b2
Mark Bell <skrambelled@gmail.com>
2024-10-03 15:36:39 -0700

    Set SelectionMode to single

--- ViewModel/MainWindowViewModel.cs
+++ ViewModel/MainWindowViewModel.cs
@@ -49,6 +49,11 @@ namespace DemoProject.ViewModel
             throw new NotImplementedException();
         }
 
+        private bool CanSave()
+        {
+            return true;
+        }
+
     }
 
 }
